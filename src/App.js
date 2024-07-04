ec0679f5f64c1c90d5644376a25d49660afabcca
law973 <lawright379@gmail.com>
2024-07-04 07:38:38 -0300

    learned about mapping arrays to elements

--- src/App.js
+++ src/App.js
@@ -8,27 +8,29 @@ class App extends Component {
     super();
 
     this.state = {
-      name: {firstName: 'Levi', lastName: 'Wright'},
-      company: 'ZTM'
-    }
+      monsters: [
+        {
+          name: 'Linda',
+        },
+        {
+          name: 'Frank',
+        },
+        {
+          name: 'Jacky',
+        },
+        {
+          name: 'Andrei',
+        },
+      ]
+    };
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name.firstName} {this.state.name.lastName} , I work at {this.state.company}</p>
-          <button onClick={() => {
-            this.setState(() => {
-              return {
-                name: {firstName: 'Sarah', lastName: 'White'},
-              };
-            });
-          }}>
-            Change Name
-          </button>
-        </header>
+        {this.state.monsters.map((monster) => {
+            return <h1>{monster.name}</h1>;
+        })}
       </div>
     );
   }
