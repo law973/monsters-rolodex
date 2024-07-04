cc4fa160acd32263b2d7ea98f891fc04b0943625
law973 <lawright379@gmail.com>
2024-07-04 13:25:48 -0300

    learned about componentDidMount

--- src/App.js
+++ src/App.js
@@ -8,25 +8,20 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Linda',
-          id: '12e1231e'
-        },
-        {
-          name: 'Frank',
-          id: '12ed2dacas'
-        },
-        {
-          name: 'Jacky',
-          id: '1231e'
-        },
-        {
-          name: 'Andrei',
-          id: '12e1e213'
-        },
-      ]
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then((users) => this.setState(() => {
+      return {monsters: users}
+    },
+    () => {
+      console.log(this.state);
+    }
+    ));
   }
 
   render() {
