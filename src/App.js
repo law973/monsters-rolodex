1625b57ca0b702992249eafedd212459e473acf1
law973 <lawright379@gmail.com>
2024-07-05 19:17:53 -0300

    learned about input search box component

--- src/App.js
+++ src/App.js
@@ -10,25 +10,36 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
+    console.log('constructor');
   }
 
   componentDidMount() {
+    console.log('componentDidMount');
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
-    .then((users) => this.setState(() => {
-      return {monsters: users}
-    },
-    () => {
-      console.log(this.state);
-    }
-    ));
+    .then((users) => 
+      this.setState(
+        () => {
+          return {monsters: users}
+        },
+        () => {
+          console.log(this.state);
+        }
+      )
+  );
   }
 
   render() {
+    console.log('render');
     return (
       <div className="App">
+        <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => {}} />
         {this.state.monsters.map((monster) => {
-            return <div key={monster.id}><h1>{monster.name}</h1></div>;
+            return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
         })}
       </div>
     );
