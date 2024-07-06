ac08cab921db620cc62a65efcf915c30c8c91705
law973 <lawright379@gmail.com>
2024-07-06 12:28:48 -0300

    learned about storing original data

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: ''
     };
     console.log('constructor');
   }
@@ -26,30 +27,32 @@ class App extends Component {
           console.log(this.state);
         }
       )
-  );
+    );
   }
 
   render() {
     console.log('render');
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
         <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => {
-          const searchString = event.target.value.toLocaleLowerCase();
-          const filteredMonsters = this.state.monsters.filter((monster) => {
-                return monster.name.toLocaleLowerCase().includes(searchString);
-          });
+          const searchField = event.target.value.toLocaleLowerCase();
           this.setState(
             () => {
-              return {monsters: filteredMonsters}
+              return {searchField}
             }
           );
         }} />
-        {this.state.monsters.map((monster) => {
+        {filteredMonsters.map((monster) => {
             return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div>
-          );
+            );
         })}
       </div>
     );
